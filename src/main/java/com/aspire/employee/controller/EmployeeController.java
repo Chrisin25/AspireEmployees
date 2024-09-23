65c94561f7c7b8f36866ea118243a2c47902db01
Manikandan <bnairmanikandan@gmail.com>
2024-09-23 15:06:18 +0530

    modified repository methods

--- src/main/java/com/aspire/employee/controller/EmployeeController.java
+++ src/main/java/com/aspire/employee/controller/EmployeeController.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/aspire/employee/repository/EmployeeRepo.java
+++ src/main/java/com/aspire/employee/repository/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.aspire.employee.repository;
 
 import com.aspire.employee.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +12,13 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
     public List<Employee> findAllByEmployeeNameStartingWith(String startsWith);
+    @Query("SELECT e FROM Employee e WHERE e.employeeId = :id AND e.managerId = 0")
+    Optional<Employee> findByIdAndManagerIdEqualsZero(@Param("id") Integer id);
 
-    Optional<Employee> findByIdAndManagerIdEqualsZero(Integer managerId);
 
-    Optional<Employee> findByStreamAndManagerIdEquals(String streamName, int i);
+    @Query("SELECT e FROM Employee e WHERE e.stream = :stream AND e.managerId = :managerId")
+    Optional<Employee> findByStreamAndManagerIdEquals(@Param("stream") String streamName, @Param("managerId") int managerId);
+
 
     List<Employee> findAllByManagerId(Integer employeeId);
 
